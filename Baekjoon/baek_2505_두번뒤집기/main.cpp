134d3a0eed1748b8b13ba243df65d20707a1c594
ji yoon <asdff2002@gmail.com>
2019-10-02 21:30:20 +0900

    Solve problems

--- Baekjoon/baek_2505_두번뒤집기/main.cpp
+++ Baekjoon/baek_2505_두번뒤집기/main.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// 이 배열이 오름차순 수열인지 반환
 bool isRight(vector<int> &arr) {
     for (int i = 0; i < arr.size(); ++i) {
         if (arr[i] != i)
@@ -31,37 +32,46 @@ int main() {
             reverse(next(arr.begin(), first), next(arr.begin(), last+1));
             ret.emplace_back(first, last);
         }
-        if (ret.size() == 2) {
+        if (ret.size() == 1 || ret.size() == 2) {
             if (isRight(arr)) {
                 for (auto &p:ret) {
                     cout << p.first+1 << " " << p.second+1 << '\n';
                 }
+                if(ret.size()==1)
+                    cout << 1 << " " << 1 << '\n';
                 return 0;
             }
-            break;
         }
+        if(ret.size()==2)
+            break;
     }
     arr = origin;
     ret.clear();
     for (int i = n-1; i >=0; --i) {
         auto it = find(arr.begin(), arr.end(), i);
         if (distance(arr.begin(), it) != i) {
-            int first = i - 1, last = distance(arr.begin(), it);
+            int first = i, last = distance(arr.begin(), it);
             if (first > last)
                 swap(first, last);
             reverse(next(arr.begin(), first), next(arr.begin(), last+1));
             ret.emplace_back(first, last);
         }
-        if (ret.size() == 2) {
+        if (ret.size() == 1 || ret.size() == 2) {
             if (isRight(arr)) {
                 for (auto &p:ret) {
                     cout << p.first+1 << " " << p.second+1 << '\n';
                 }
+                if(ret.size()==1)
+                    cout << 1 << " " << 1 << '\n';
                 return 0;
             }
-            break;
+            if(ret.size()==2)
+                break;
         }
     }
-    cout << 1 << " " << n << '\n' << 1<< " " << n << '\n';
+
+    for(int i=0; i<2; ++i) {
+        cout << 1 << " " << 1 << '\n';
+    }
     return 0;
 }
