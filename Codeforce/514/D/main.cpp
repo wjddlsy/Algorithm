b5d5ee232d61219f6db7d7ab80377376c5915876
ji yoon <asdff2002@gmail.com>
2019-10-17 21:10:57 +0900

    Solve problems

--- Codeforce/514/D/main.cpp
+++ Codeforce/514/D/main.cpp
@@ -8,48 +8,72 @@ int n, m, k;
 
 void computeLog() {
     log[1] = 0;
-    for(int i=2; i<n; ++i) {
-        log[i] = log[i/2] + 1;
+    for (int i = 2; i <= n; ++i) {
+        log[i] = log[i / 2] + 1;
     }
 }
 
-int main() {
-    cin>>n>>m>>k;
+    int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n >> m >> k;
     vector<vector<int>> droids(n);
 
-    for(int i=0; i<n; ++i) {
-        for(int j=0; j<m; ++j) {
-            int a; cin>>a;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            int a;
+            cin >> a;
             droids[i].emplace_back(a);
         }
     }
 
     // sparse table
-    for(int i=0; i<m; ++i) {
-        for(int j=0; j<n; ++j) {
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
             st[i][j][0] = droids[j][i];
         }
     }
 
-    for(int mm=0; mm<m; ++mm) {
-        for(int i=0; i<n; ++i) {
-            for(int j=1; i+(1<<j)<=n; ++j) {
-                st[mm][i][j] = max(st[mm][i][j-1], st[mm][i+(1<<j)][j-1]);
+    for (int mm = 0; mm < m; ++mm) {
+        for (int j = 1; j <20; ++j) {
+            for (int i = 0; i + (1 << j) <= n; ++i) {
+                st[mm][i][j] = max(st[mm][i][j - 1], st[mm][i + (1 << (j-1))][j - 1]);
             }
         }
     }
 
     computeLog();
 
-    int left=0, right=0;
+    int left = 0, right = 0, ret = 0;
+    vector<int> info(m, 0);
 
-    while(right<n) {
-        for(int i=0; i<m; ++i) {
-            int dist = right-left+1;
-            int maximum = max(st[i][left][dist], st[i][right-(1<<dist)][dist]);
+    while (left < n && right < n) {
+        int sum = 0;
+        vector<int> tmp(m, 0);
+        for (int i = 0; i < m; ++i) {
+            int dist = log[right - left + 1];
+            int maximum = max(st[i][left][dist], st[i][right - (1 << dist) + 1][dist]);
+            sum += maximum;
+            tmp[i] = maximum;
         }
+        if(sum <=k ) {
+            if(ret <= right-left) {
+                info = tmp;
+                ret = right-left;
+            }
+            right++;
+        }
+        else {
+            left++;
+            if(left > right)
+                right = left;
+        }
+
     }
 
-    std::cout << "Hello, World!" << std::endl;
+    for(auto &i:info) {
+        cout << i << " ";
+    }
+    //std::cout << "Hello, World!" << std::endl;
     return 0;
 }
