146c3552d4dc253855fc0068f0dbf8220415d57b
ji yoon <asdff2002@gmail.com>
2020-01-12 23:03:42 +0900

    Solve problems

--- Baekjoon/baek_1051_숫자정사각형/main.cpp
+++ Baekjoon/baek_1051_숫자정사각형/main.cpp
@@ -8,11 +8,24 @@ string board[50];
 int main() {
     int N, M;
     cin >> N >> M;
-    for(int i=0; i<N; ++i) {
+    for (int i = 0; i < N; ++i) {
         cin >> board[i];
     }
 
+    int ret = 1;
+    for (int size = 2; size <= min(N, M); ++size) {
+        for (int i = 0; i <= N - size; ++i) {
+            for (int j = 0; j <= M - size; ++j) {
+                if (board[i][j] == board[i + size - 1][j] &&
+                board[i][j] == board[i][j + size - 1] && board[i][j] == board[i + size - 1][j + size - 1]) {
+                    ret = size * size;
+                }
+            }
+        }
+    }
 
-    std::cout << "Hello, World!" << std::endl;
+    cout << ret;
+
+    //std::cout << "Hello, World!" << std::endl;
     return 0;
 }
